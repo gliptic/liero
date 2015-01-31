86e2d04a7a0ca422788d4b92bba3a914b96f93a3
Anders Höckersten <anders@hockersten.se>
2015-01-31 21:41:17 +0100

    Don't use single screen replay setting from replay

--- controller/replayController.cpp
+++ controller/replayController.cpp
@@ -159,7 +159,7 @@ void ReplayController::changeState(State newState)
 	
 	if(newState == StateGame)
 	{
-		if (game->settings->singleScreenReplay) {
+		if (gfx.settings->singleScreenReplay) {
 			game->clearViewports();
 			game->addViewport(new Viewport(gvl::rect(0, 0, 504, 350), game->worms[0]->index, 100, 504, 350));
 		}
