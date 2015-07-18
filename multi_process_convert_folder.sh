128fd095b5c16c49f0b28858c96756341de41427
Anders Höckersten <anders@hockersten.se>
2015-07-18 18:44:30 +0200

    Suffix spectator and normal replays for uniqueness

--- multi_process_convert_folder.sh
+++ multi_process_convert_folder.sh
@@ -3,4 +3,4 @@
 # use as: ./multi_process_convert_folder <folder_name> <number_of_parallell_jobs>
 
 find "$1" -type f -name \*.lrp -print0 | parallel -0 -j$2 ./videotool -r
-find "$1" -type f -name \*.lrp -print0 | parallel -0 -j$2 ./videotool -r
+find "$1" -type f -name \*.lrp -print0 | parallel -0 -j$2 ./videotool -s -r
