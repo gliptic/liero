4f673e361be0d03f7f830ce453114c800b95f68a
Anders Höckersten <anders@hockersten.se>
2015-07-20 12:30:48 +0200

    Nicer multi process script

--- multi_process_convert_folder.sh
+++ multi_process_convert_folder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # For all files in a directory, creates both spectator and normal replays in parallel
 # use as: ./multi_process_convert_folder <folder_name> <number_of_parallell_jobs>
-
-find "$1" -type f -name \*.lrp -print0 | parallel -0 -j$2 ./videotool -r
-find "$1" -type f -name \*.lrp -print0 | parallel -0 -j$2 ./videotool -s -r
+echo Now creating split screen videos
+find "$1" -type f -name \*.lrp -print0 | parallel --no-notice -0 -j$2 ./videotool -r
+echo Now creating spectator mode videos
+find "$1" -type f -name \*.lrp -print0 | parallel --no-notice -0 -j$2 ./videotool -s -r
